e74dc0e3d592dded4bc6b6bc13cfb6119828eda3
Boyan Iliev <97385003+Boyan253@users.noreply.github.com>
2023-01-25 18:26:19 +0200

    Add files via upload

---
+++ Exam Preparation/SharedTrips/middleware/guards.js
@@ -0,0 +1,44 @@
+
+
+function isUser() {
+    return function (req, res, next) {
+        if (req.session.user) {
+            next()
+        } else {
+            res.redirect('/login')
+        }
+    }
+
+}
+
+function isGuest() {
+    return function (req, res, next) {
+
+        if (req.session.user) {
+            res.redirect('/')
+        } else {
+            next()
+        }
+
+
+    }
+}
+
+function isOwner() {
+  return function (req, res, next) {
+const userId = req.session.user?._id;
+//TODO change property name to match the collection
+if (res.locals.trip.owner._id == userId) {
+    next()
+}else{
+res.redirect('/login')
+}
+    }
+}
+
+
+module.exports = {
+    isGuest,
+    isUser,
+    isOwner
+}
